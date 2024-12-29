237cf00bedbee38212b8e977f8338034a5bc6b42
missfangfang <tiffanyfang67@gmail.com>
2024-12-28 17:47:52 -0800

    [fix] Remove Tailwind from monorepo level

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-    return <>Hello</>;
+    return <div>Hello</div>;
 }
 
 export default App;
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./apps/web/index.html', './apps/web/src/**/*.{js,ts,jsx,tsx}'],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {}
     },
